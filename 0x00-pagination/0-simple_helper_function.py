629e2e71493c80b1bc91536cfcc75aebc75b82b1
besthor <igbebestor7@gmail.com>
2023-07-26 12:43:47 +0000

    fixed pycodestyle error

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-""" Description: function named index_range that takes two integer arguments page and page_size"""
+""" Description: function named index_range 
+    that takes two integer arguments page and page_size
+"""
 
 from typing import Tuple
 
