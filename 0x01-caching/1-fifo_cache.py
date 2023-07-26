202ce8d514c4059a97229dab5e462ddc22ed950e
besthor <igbebestor7@gmail.com>
2023-07-26 13:13:23 +0000

    LIFO Caching

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Create FIFOCache class that inherits from BaseCaching"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ Define FIFOCache """
+
+    def __init__(self):
+        """ Initialize FIFOCache """
+        self.queue = []
+        super().__init__()
+
+    def put(self, key, item):
+        """ Assign the item to the dictionary """
+        if key and item:
+            if self.cache_data.get(key):
+                self.queue.remove(key)
+            self.queue.append(key)
+            self.cache_data[key] = item
+            if len(self.queue) > self.MAX_ITEMS:
+                delete = self.queue.pop(0)
+                self.cache_data.pop(delete)
+                print('DISCARD: {}'.format(delete))
+
+    def get(self, key):
+        """ Return the value associated with the given key """
+        return self.cache_data.get(key)
